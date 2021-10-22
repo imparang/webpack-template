a8eedae9314fa3850851fba812266e359ba1a1ab
imparang <jeongjaeyn@gmail.com>
2021-10-22 19:20:08 +0900

    fix: Fix bugs in deploying netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,35 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
-  console.log(env, options)
-  return { 
-    resolve: {
-      extensions: ['.js'],
-      alias: {
-        '~': path.resolve(__dirname, 'src')
-      }
-    }, 
+  return {
     entry: './src/main.js',
     output: {
+      //path: '',
+      filename: 'core.js',
       publicPath: '/',
-      clean: true
+      clean: true,
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: 'babel-loader'
+          use: 'babel-loader',
         },
         {
           test: /\.s?css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-            'postcss-loader',
-            'sass-loader'
-          ]
-        }
-      ]
+          use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+      ],
     },
     plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
+      new HtmlPlugin({
+        template: './src/index.html',
       }),
       new CopyPlugin({
-        patterns: [
-          { from: 'static' } 
-        ]
-      })
+        patterns: [{ from: 'static' }],
+      }),
     ],
-    // devServer: {
-    //   port: 8080,
-    //   open: true,
-    //   historyApiFallback: true
-    // }
-  }
-}
+  };
+};
